f98687e9716f3ce47bf42c3bb12b3f1accea021b
ramses-ayala <ramses0595@gmail.com>
2023-09-05 12:18:55 -0500

    adjustments

--- src/App.js
+++ src/App.js
@@ -9,49 +9,39 @@ import Task from './components/Task';
 
 function App() {
 
-  
-  // CHECAR SI EXISTE LA CLAVE 'citas' en el localStorage
   let taskInit = JSON.parse(localStorage.getItem("tasks"));
   
   if(!taskInit) {
     taskInit = []; 
   }
-  // ARREGLO DE CITAS
+  
+
   let [tasks, saveTasks] = useState(taskInit);
 
-
-  // use efect para realizar ciertas operaciones cuando el state cambia
   useEffect(() => {
-    //console.log("DOCUMENTO LISTO O SE ACTUALIZO EL STATE CITAS");
     if(taskInit){
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }else{
-      //console.log("ENTRO AL ELSE");
       localStorage.setItem('tasks', JSON.stringify([]));
     }
   },[tasks, taskInit]);
 
-
-
-  // GUARDAR CITA EN EL STATE DE CITAS
   const create = (task) => {
     saveTasks([...tasks, task]);
-    //console.log("ESTE LA CITA DESDE EL COM. App ", cita);
   }
 
   const updateTask = (updatedTask) => {
     saveTasks((prevTasks) => prevTasks.map((prevTask) => (prevTask.id === updatedTask.id ? updatedTask : prevTask)));
   }
 
-  // ELIMINAR CITA DE CITAS POR ID
   const deleteTask = (id) => {
 
-    const restTasks = tasks.filter(cita => {
-      return cita.id !== id;
+    const restTasks = tasks.filter(task => {
+      return task.id !== id;
     });
     
     saveTasks(restTasks);
-    //console.log("SE ELIMINARA EL: ",id);
+    
   }
 
   return (
